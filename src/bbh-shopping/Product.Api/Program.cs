207a473201c8b26520f84982d464f5f894aad6ff
han.zhou@zuru.com <han.zhou@zuru.com>
2025-01-15 17:04:09 +0800

    1、修改启动配置文件 2、添加swagger服务

--- src/bbh-shopping/Product.Api/Program.cs
+++ src/bbh-shopping/Product.Api/Program.cs
@@ -5,9 +5,19 @@ namespace Product.Api
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            // 添加 Swagger 服务
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
+            // 使用 Swagger 中间件
+            app.UseSwagger();
+            app.UseSwaggerUI();
+
             app.MapGet("/", () => "Hello World!");
+
 
             app.Run();
         }
